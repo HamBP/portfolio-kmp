8d635d9345a7b4c12914ab8741123277f813d8c9
algosketch <algosketch@gmail.com>
2024-04-09 01:41:06 +0900

    style : 600 폰트 추가

--- composeApp/src/wasmJsMain/kotlin/presentation/ui/type.kt
+++ composeApp/src/wasmJsMain/kotlin/presentation/ui/type.kt
@@ -2,6 +2,7 @@ package presentation.ui
 
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.platform.Font
 import androidx.compose.ui.unit.sp
 import pretendardRegular
@@ -13,18 +14,26 @@ import kotlin.io.encoding.ExperimentalEncodingApi
 val pretendardFamily = FontFamily(
     Font(
         identity = "Pretendard-Regular",
-        data = Base64.decode(pretendardRegular)
-    )
+        data = Base64.decode(pretendardRegular),
+        weight = FontWeight.Normal,
+    ),
+    Font(
+        identity = "Pretendard-Semibold",
+        data = Base64.decode(pretendardSemiBold),
+        weight = FontWeight.SemiBold
+    ),
 )
 
 val h2 = TextStyle(
     fontFamily = pretendardFamily,
     fontSize = 28.sp,
+    fontWeight = FontWeight.SemiBold,
 )
 
 val h3 = TextStyle(
     fontFamily = pretendardFamily,
     fontSize = 16.sp,
+    fontWeight = FontWeight.SemiBold,
 )
 
 val body2 = TextStyle(
